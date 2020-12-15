56f7f1ad5a9ea13c52becca00d86a47183464324
Samsara <56559825+Samsara-GOG@users.noreply.github.com>
2020-12-15 18:45:30 +0100

    Suppression des commentaires de l'exo
    Car il existe déà un README.MD fait à ce propos pour décrire le cahier des charges.

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// Exo - Les fondamentaux
-
-// Consignes: Reproduire ce formulaire en HTML/CSS + ReactJS
-
-// A la soumission du formulaire, ajouter les données entrantes dans un tableau "comments" situé dans l'objet state.
-// Ex: 
-// comments: [  
-// { name: "JB", message: "Youhou la famille!"},
-// { name: "Kirikou", message: "Je ne suis pas grand mais je suis vaillant"}
-//  ]
-
-// a rendre: lien git https://classroom.google.com/u/0/c/MTg2OTY1MDMwNDc0/a/MjE0NjI4ODA5NDQx/details
-
 class App extends React.Component {
 
     state = {
